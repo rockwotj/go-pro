2b2c49e5b566e5e9098c2a4b6191b921ad12751d
rockwotj <rockwotj@rose-hulman.edu>
2015-02-15 19:40:28 -0500

    changed hw

--- final-homework-assignment/problem.go
+++ final-homework-assignment/problem.go
@@ -1,5 +1,17 @@
 package main
 
+// Summary of points:
+// 1 point for coding style
+// 1 point for defining a struct for milk that 'implements' groceryItem
+// 1 point for a function to average a grocery list
+// 1 point for a function to total a grocery list
+// 1 point for a function to find the maximal priced item in a grocery list
+// 1 point for making a channel for each of the above functions
+// 4 points for running the above 3 functions all at once 
+//   (for 2 points calculate the values sequentially 
+//      - you'll have to comment out the select statement for this to work)
+
+
 import(
 	"fmt"
 )
@@ -13,16 +25,6 @@ type fruit struct {
 	price float64
 }
 
-type meat struct {
-	pounds float64
-	pricePerPound float64
-}
-
-type milk struct {
-	gallons float64
-	pricePerGallon float64	
-}
-
 func (f fruit) getPrice() float64 {
 	if f.fresh {
 		return f.price
@@ -30,12 +32,54 @@ func (f fruit) getPrice() float64 {
 		return 0
 	}
 }
+
+type meat struct {
+	pounds float64
+	pricePerPound float64
+}
+
 func (m meat) getPrice() float64 {
 	return m.pounds * m.pricePerPound
 }
-// START SOLUTION
 
-// END SOLUTION
+// START PROBLEM
+
+// define a struct for milk with fields: gallons and pricePerGallon as both float64 types (1 point)
+
+// Define a function to calculate the price of milk via: gallons * pricePerGallon
+
+func groceryListStats(groceryList []groceryItem, r chan result) {
+	stats := result{}
+	// Open up a float 64 channel for each of the goroutines (1 point)
+	totalResult := nil
+	averageResult := nil
+	maxResult := nil
+
+	// Start each of the go routines (4 points)
+	
+	fmt.Println("Started All Calcs")
+	for i := 0; i < 3; i++ {
+		select {
+			case stats.total = <- totalResult:
+				fmt.Println("Recieved Total")
+			case stats.average = <- averageResult:
+				fmt.Println("Recieved Average")
+			case stats.max = <- maxResult:
+				fmt.Println("Recieved Max")
+		}
+	}
+	r <-stats
+}
+
+// Write a function that totals the prices of all items in a grocery list (1 point)
+
+// Write a function that finds the maximal price of an item in a grocery list (1 point)
+
+// Write a function that finds the average of an item in a grocery list (1 point)
+// You might find the methods len() [to find the length of a slice]
+// and float64() [to convert an int to a float64] to be useful.
+
+// END PROBLEM
 	
 type result struct {
 	average float64
@@ -56,7 +100,7 @@ func main() {
 		fruit{fresh:true, price:2.33},
 		meat{pounds:3.3, pricePerPound:1.61},
 		meat{pounds:1.2, pricePerPound:24.1},
-		milk{gallons:.5, pricePerGallon: 4.33},
+		milk{gallons:.5, pricePerGallon: 4.33}, // MAKE SURE THIS MATCHES YOUR STRUCT
 	}
 	channel := make(chan result)
 	go groceryListStats(groceryList, channel)
