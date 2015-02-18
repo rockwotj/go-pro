a51670b3565854ee4c472890fa0f3e5b9ac706ec
rockwotj <rockwotj@rose-hulman.edu>
2015-02-18 13:22:37 -0500

    Finished basic server that takes and uploads photos, need to integrate with everything else and add a results page

--- project/src/project/server/server.go
+++ project/src/project/server/server.go
@@ -17,8 +17,17 @@ type page struct {
 	Body  string
 }
 
-var photosUploaded = 0
 
+func Start() {
+    http.HandleFunc("/receiveUrl", downloadHandler)
+    http.HandleFunc("/receive", uploadHandler)
+    http.HandleFunc("/submit", makeHandler("submit.html", "unknown! The project is not finished yet, check back later"))
+    http.HandleFunc("/", makeHandler("index.html", "Please submit your photo, and we'll tell you if it is a sunset or not!"))
+    http.ListenAndServe(":8080", nil)
+}
+
+var photosUploaded = 0
+var jipsUploaded = 0
 var templates = template.Must(template.ParseFiles("./static/index.html", "./static/submit.html"))
 
 func makeHandler(templateName string, body string) func(http.ResponseWriter, *http.Request) {
@@ -30,6 +39,40 @@ func makeHandler(templateName string, body string) func(http.ResponseWriter, *ht
 	}
 }
 
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+        photosUploaded++
+        //Call to ParseForm makes form fields available.
+        r.ParseForm()
+        rawURL := r.PostFormValue("url")
+        fmt.Println("Received URL: ", rawURL);
+        uploadedPath := "./tmp/uploadedfile" + strconv.Itoa(photosUploaded) + ".jpg"
+        file, err := os.Create(uploadedPath)
+        if err != nil {
+            fmt.Println(err)
+            panic(err)
+        }
+        defer file.Close()
+        check := http.Client{
+            CheckRedirect: func(r *http.Request, via []*http.Request) error {
+                r.URL.Opaque = r.URL.Path
+                return nil
+            },
+        }
+        resp, err := check.Get(rawURL) // add a filter to check redirect
+        if err != nil {
+            fmt.Println(err)
+            panic(err)
+        }
+        defer resp.Body.Close()
+        fmt.Println(resp.Status)
+        _, err = io.Copy(file, resp.Body)
+        if err != nil {
+            panic(err)
+        }
+        fmt.Fprintf(w, "File uploaded successfully : ")
+        fmt.Fprintf(w, rawURL)
+ }
+
  func uploadHandler(w http.ResponseWriter, r *http.Request) {
  		photosUploaded++
         // the FormFile function takes in the POST input id file
@@ -38,7 +81,6 @@ func makeHandler(templateName string, body string) func(http.ResponseWriter, *ht
             fmt.Fprintln(w, err)
             return
         }
-        defer file.Close()
         uploadedPath := "./tmp/uploadedfile" + strconv.Itoa(photosUploaded)
         if strings.HasSuffix(header.Filename, ".zip") {
         	uploadedPath += ".zip"
@@ -53,7 +95,6 @@ func makeHandler(templateName string, body string) func(http.ResponseWriter, *ht
             fmt.Fprintf(w, "Unable to create the file for writing. Check your write access privilege")
             return
         }
-        defer out.Close()
         // write the content from POST to the file
         _, err = io.Copy(out, file)
         if err != nil {
@@ -61,14 +102,13 @@ func makeHandler(templateName string, body string) func(http.ResponseWriter, *ht
         }
         fmt.Fprintf(w, "File uploaded successfully : ")
         fmt.Fprintf(w, header.Filename)
+        file.Close()
+        out.Close()
+        if strings.HasSuffix(header.Filename, ".zip") {
+        	unzip(uploadedPath)
+            os.RemoveAll(uploadedPath)
+        }
  }
-
-func Start() {
-	http.HandleFunc("/receive", uploadHandler)
-	http.HandleFunc("/submit", makeHandler("submit.html", "unknown! The project is not finished yet, check back later"))
-	http.HandleFunc("/", makeHandler("index.html", "Please submit your photo, and we'll tell you if it is a sunset or not!"))
-	http.ListenAndServe(":8080", nil)
-}
 
 func unzip(zipfile string) {
     reader, err := zip.OpenReader(zipfile)
@@ -77,6 +117,9 @@ func unzip(zipfile string) {
         return
     }
     defer reader.Close()
+    jipsUploaded++
+    newDir := "./tmp/images" + strconv.Itoa(jipsUploaded) +"/"
+    os.MkdirAll(newDir, 0777)
     for _, f := range reader.Reader.File {
         zipped, err := f.Open()
         if err != nil {
@@ -95,12 +138,29 @@ func unzip(zipfile string) {
                 fmt.Println(err)
                 return
         	}
-            defer writer.Close()
+            
             if _, err = io.Copy(writer, zipped); err != nil {
                 fmt.Println(err)
                 return
             }
+        writer.Close()
         fmt.Println("Decompressing : ", path)
+        photosUploaded++
+        copyFile(filepath.Join(newDir, f.Name), path)
+        os.Remove(path)
         }
     }
+}
+
+func copyFile(dst, src string) error {
+    in, err := os.Open(src)
+    if err != nil { return err }
+    defer in.Close()
+    out, err := os.Create(dst)
+    if err != nil { return err }
+    defer out.Close()
+    _, err = io.Copy(out, in)
+    cerr := out.Close()
+    if err != nil { return err }
+    return cerr
 }
